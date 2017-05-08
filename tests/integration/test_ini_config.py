943f05022b7e66bad4f795391d5e2ba45f1a3384
Artur Maciag <artur@MacBook-Pro-Artur.local>
2017-05-08 13:27:44 +0200

    config options upper names fix

--- tests/integration/test_ini_config.py
+++ tests/integration/test_ini_config.py
@@ -37,6 +37,13 @@ format = %(asctime)s %(levelname)-5.5s [%(name)s][%(threadName)s] %(message)s
             yield f
 
     @pytest.yield_fixture
+    def ini_file_upper(self):
+        with tempfile.NamedTemporaryFile(suffix='.ini') as f:
+            f.write(b('[TESTING]\nKEY = value'))
+            f.flush()
+            yield f
+
+    @pytest.yield_fixture
     def ini_file_with_logging(self, ini_file, logging_config):
         ini_file.write(logging_config)
         ini_file.flush()
@@ -84,6 +91,19 @@ format = %(asctime)s %(levelname)-5.5s [%(name)s][%(threadName)s] %(message)s
         config = config_factory.create(config_type, **config_opt)
 
         assert config['TESTING']['key'] == 'value'
+
+    def test_section_option_upper(self, ini_file_upper):
+        config_type = 'ini'
+        config_opt = {
+            'ini_file': ini_file_upper.name,
+            'bar': 'baz',
+        }
+
+        config = config_factory.create(config_type, **config_opt)
+
+        assert dict(config['TESTING']) == {
+            'KEY': 'value',
+        }
 
     def test_get_value(self, ini_file):
         config_type = 'ini'
